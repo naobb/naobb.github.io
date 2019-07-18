223b89ddd33917fd1c44acdee032f2978f73498c
nao <naoblabo@gmail.com>
2019-07-19 08:12:09 +0900

    blogリストのデザイン

--- src/.vuepress/components/PostList.vue
+++ src/.vuepress/components/PostList.vue
@@ -1,13 +1,14 @@
 <template>
-    <div>
-        <div v-for="post in posts">
-            <h2><a v-bind:href="post.path">{{post.title}}</a></h2>
-            <p>{{post.frontmatter.description}}</p>
-            <a v-bind:href="post.path">続きを読む</a>
+    <div id="content">
+        <div v-for="post in posts" class="card card-body">
+            <h2 class="card-title"><a v-bind:href="post.path">{{post.title}}</a></h2>
+            <small>{{post.frontmatter.date || dateFormatter}}</small>
+            <a v-bind:href="post.path" id="more">続きを読む</a>
         </div>
     </div>
 </template>
 <script>
+import format from 'date-fns/format'
 export default {
     computed: {
         posts() {
@@ -15,8 +16,32 @@ export default {
             // blogディレクトリ以下を投稿記事一覧表示の対象とする
             .filter(post => post.path.startsWith('/blog/'))
             // dateに設定した日付の降順にソートする
-            .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+            .sort((b, a) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
         }
+    },
+     filters: {
+    dateFormatter: function (date) {
+      return format(date, 'YYYY/MM/DD')
     }
+  }
 }
 </script>
+
+<style>
+#content {
+  float: both;
+  width: 600px;
+}
+#more {
+  display: block;
+  float: right;
+}
+
+.card {
+  width: 35%;
+  margin-top: 20px;
+  margin-right: 10px;
+  margin-bottom: 20px;
+  float: left;
+}
+</style>
--- src/index.md
+++ src/index.md
@@ -6,5 +6,5 @@
   <ImageCarousel />
 </ClientOnly>
 
-<h3>最近のブログ投稿はこちら</h3>
+<h3 style="color:#3eaf7c;">最近のブログ投稿はこちら</h3>
 <PostList />
